a2c9ecf16b7bf001bc8b256d129b5db17efd64ac
Leto D-Art <goncharoff.k2018@yandex.ru>
2023-04-02 20:28:18 +0300

    connected to the camera

--- app/src/main/java/com/example/recorder/ui/MainWindow/MainWindowFragment.kt
+++ app/src/main/java/com/example/recorder/ui/MainWindow/MainWindowFragment.kt
@@ -1,8 +1,14 @@
 package com.example.recorder.ui.MainWindow
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,7 +18,9 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.recorder.R
 import com.example.recorder.databinding.FragmentMainWindowBinding
 import com.example.recorder.utils.launchAndCollectIn
+import com.example.recorder.utils.toast
 import okhttp3.WebSocket
+import timber.log.Timber
 
 
 class MainWindowFragment : Fragment(R.layout.fragment_main_window) {
@@ -38,6 +46,8 @@ class MainWindowFragment : Fragment(R.layout.fragment_main_window) {
             Lifecycle.State.RESUMED
         )
 
+        requestPermission()
+
         mainWindowViewModel.userFlow.launchAndCollectIn(viewLifecycleOwner) { user ->
             user?.user_name.let {
                 app.supportActionBar?.setTitle(it)
@@ -56,5 +66,50 @@ class MainWindowFragment : Fragment(R.layout.fragment_main_window) {
         socketViewModel.messageFlow.launchAndCollectIn(viewLifecycleOwner) { message ->
             binding.textView2.text = message
         }
+    }
+
+
+    private fun requestPermission() {
+        requestCameraPermissionIfMissing { granted ->
+            if (granted)
+                startCamera()
+            else
+                toast(R.string.add_permission_for_camera)
+        }
+    }
+
+    private fun startCamera() {
+        val processCameraProvider = ProcessCameraProvider.getInstance(requireContext())
+        processCameraProvider.addListener({
+            try {
+                val cameraProvider = processCameraProvider.get()
+                val previewUseCase = buildPreviewUseCase()
+
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(requireActivity(), CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase)
+            } catch (e: Exception) {
+                Timber.tag("Camera").d(e)
+            }
+
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun buildPreviewUseCase(): Preview {
+        return Preview.Builder().build().also { it.setSurfaceProvider(binding.cameraPreview.surfaceProvider) }
+    }
+
+    private fun requestCameraPermissionIfMissing(onResult: ((Boolean) -> Unit)) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            onResult(true)
+        else
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                onResult(it)
+            }.launch(android.Manifest.permission.CAMERA)
     }
 }
